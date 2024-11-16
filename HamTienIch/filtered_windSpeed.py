ba22d313e1702cf5315367ccfb90b0511bd22f0b
1105trxc <trackhoa1105@gmail.com>
2024-11-16 13:31:09 +0700

    Chuyen cac ham loc vao chung thu muc HamTienIch, Tao thu muc CRUD, chuyen 2 file CD RU vao

---
+++ HamTienIch/filtered_windSpeed.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+file_path = 'D:\\hocTap\\CODE\\doAnPytthon\\weather_classification_data.csv' 
+output_file = 'D:\\hocTap\\CODE\\doAnPytthon\\output.csv'
+
+try:
+    data = pd.read_csv(file_path)
+except FileNotFoundError:
+    print("File không tồn tại. Hãy kiểm tra lại đường dẫn.")
+    exit()
+
+# Nhập khoảng giá trị từ người dùng
+try:
+    min_pressure = float(input("Nhập giá trị tối thiểu của Wind Speed (mph): "))
+    max_pressure = float(input("Nhập giá trị tối đa của Wind Speed (mph): "))
+except ValueError:
+    print("Vui lòng nhập số hợp lệ.")
+    exit()
+
+# Lọc dữ liệu theo khoảng giá trị
+filtered_data = data[(data['Wind Speed'] >= min_pressure) & 
+                     (data['Wind Speed'] <= max_pressure)]
+
+# Kiểm tra nếu không có dữ liệu nào thỏa mãn
+if filtered_data.empty:
+    print("Không có dữ liệu nào trong khoảng đã chỉ định.")
+else:
+    # Xuất dữ liệu đã lọc ra file CSV
+    filtered_data.to_csv(output_file, index=False)
+    print(f"Dữ liệu đã lọc được lưu vào file: {output_file}")
+   
